a415ad2778bfae2a44eb043959bf9c4dedceafd2
Takkunkuma <102077650+Takkunkuma@users.noreply.github.com>
2025-03-09 17:42:03 -0700

    Initial Commit

---
+++ gohunmae_mvp/gohunmae_mvpApp.swift
@@ -0,0 +1,17 @@
+//
+//  gohunmae_mvpApp.swift
+//  gohunmae_mvp
+//
+//  Created by Tatsuo Kumamoto on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct gohunmae_mvpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
