8f96ed6a5abaaa50d87f22cae16929fe0f3cf1c9
Takkunkuma <102077650+Takkunkuma@users.noreply.github.com>
2025-04-27 15:01:37 -0700

    add black frame and triangle around the clock; clicking the triangle slides the frame up and down

--- gohunmae_mvp/Time/TimeView.swift
+++ gohunmae_mvp/Time/TimeView.swift
@@ -14,6 +14,8 @@ struct TimeView: View {
     @State private var timeString = timeController.getTimeString(date: Date())
     @State private var delay: String = "0"
     @State private var isShowingTimeView = true
+    @State private var isSlidDown = true
+    @State private var rotationAngle: Double = 180
     private var timer = Timer.publish(every: 1, tolerance: 0.2, on: .main, in: .common)
         .autoconnect()
     
@@ -28,33 +30,56 @@ struct TimeView: View {
                         let futureDate = timeNow.addingTimeInterval(Double(delay)! * 60) // Add 5 seconds
                         timeString = timeController.getTimeString(date: futureDate)
                     }
-                Spacer()
-                Picker("Choose Time to Delay", selection: $delay) {
-                    ForEach(timeController.getTimeChoices(), id: \.self) {
-                        Text($0.description)
+                ZStack(alignment: .topTrailing){
+                    VStack{
+                        Picker("Choose Time to Delay", selection: $delay) {
+                            ForEach(timeController.getTimeChoices(), id: \.self) {
+                                Text($0.description)
+                            }
+                        }
+                        .padding(20)
+                        .pickerStyle(.wheel)
+                        .frame(width: 300, height: 150)
+                        .clipped()
+                        
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Text("Start!")
+                                .font(.title)
+                                .bold()
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity) // Makes the button stretch horizontally
+                                .padding()
+                                .background(Color.black)
+                                .cornerRadius(15)
+                        })
+                        .padding(20)
+                        .frame(width: 275)
+                        .shadow(color: .gray.opacity(0.6), radius: 5, x: 0, y: 5)
+                    }
+                    .background(
+                        RoundedRectangle(cornerRadius: 10.0)
+                            .stroke(Color.black, lineWidth: 2)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                    )
+                    
+                    
+                    Button(action: {
+                        withAnimation {
+                            isSlidDown.toggle()
+                            rotationAngle -= 180
+                        }
+                    }) {
+                        ZStack {
+                                Image("black_triangle")
+                                    .rotationEffect(Angle(degrees: rotationAngle))
+                            }
+                        .offset(x: 10, y: -10)
                     }
                 }
-                .pickerStyle(.wheel)
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Text("Start!")
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity) // Makes the button stretch horizontally
-                        .padding()
-                        .background(Color.black)
-                        .cornerRadius(15)
-                })
-                .padding(.horizontal, 40)
-                .shadow(color: .gray.opacity(0.6), radius: 5, x: 0, y: 5)
-//                NavigationLink(destination: SettingView(), isActive: $isShowingTimeView ) {
-//                    Image(systemName: "gear")
-//                        .foregroundStyle(Color(UIColor(named: "IconColor")!))
-//                }
-                Spacer()
-                
+                .offset(y: isSlidDown ? 260 : 0) // <-- Move it down when toggled
+                .animation(.easeInOut, value: isSlidDown)
             }
-            .padding()
         }
     }
 }
